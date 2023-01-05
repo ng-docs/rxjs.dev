ae9622ef6d22ac23f2ae41682fd41207ec0ec2af
Zhicheng WANG <zcwang@thoughtworks.com>
2023-01-05 21:30:22 +0800

    fix: 更新发布脚本

--- deploy-cn.sh
+++ deploy-cn.sh
@@ -11,7 +11,11 @@ cp -r dist/* prebuilt/
 
 cd prebuilt
 
+nt mark './generated/**/*.json'
+
 cp index.html 404.html
+
+ossutil cp -i "$AYID" -k "$AYPD" -r . oss://rxjs-8 -e oss-cn-hangzhou.aliyuncs.com -u
 
 commitSha=$(git rev-parse --short HEAD)
 commitMessage=$(git log --oneline -n 1)
@@ -19,7 +23,7 @@ commitMessage=$(git log --oneline -n 1)
 git add .
 git commit --allow-empty -am "${commitMessage}" || true
 
-git push release release:master
+git push origin master
 
 set +e
 set +x
