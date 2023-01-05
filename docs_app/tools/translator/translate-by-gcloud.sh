7be50bab88a0d62499dd6a2ab3a14af8e662c741
Zhicheng WANG <zcwang@thoughtworks.com>
2023-01-05 22:36:58 +0800

    fix: 修正翻译脚本

--- docs_app/tools/translator/translate-by-gcloud.sh
+++ docs_app/tools/translator/translate-by-gcloud.sh
@@ -1,16 +1,4 @@
 #!/usr/bin/env sh
 
-# markdown 文档
-nt translate content/**/*.md --engine=gcloud
-
-# html 文档
-nt translate content/**/*.html --engine=gcloud
-
-# 导航菜单
-nt translate content/navigation.json --engine=gcloud
-
-cd ..
-# 源码
-nt translate 'packages/**/*.ts' '!packages/**/*.d.ts' '!packages/**/*_spec.ts' '!packages/**/*.spec.ts' -et internal -et nodoc --engine=gcloud
-
-cd -
+# 词典
+nt translate tools/translator/dict/rxjs.sqlite --engine=gcloud
